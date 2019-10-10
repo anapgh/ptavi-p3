852c5152f68f8000bd8cc5d15e3f366968098262
anapgh <ap.garcia.2016@alumnos.urjc.es>
2019-10-10 13:14:32 +0200

    llama a la clase y manejador de smallsmilhandler e imprime la lista

--- karaoke.py
+++ karaoke.py
@@ -6,22 +6,26 @@ Created on Thu Oct 10 11:43:08 2019
 @author: ana
 """
 
-from xml.sax import make_parser
-from xml.sax.handler import ContentHandler
 import smallsmilhandler
 import sys
 
+
+       
+        
 if __name__ == "__main__":
     if len(sys.argv) != 2 :
         sys.exit("Usage: python3 karaoke.py file.smil")
         
-    ficherosmil = sys.argv[1]
-    comprobacion = ficherosmil.split('.')
+    fichero = sys.argv[1]
+    comprobacion = fichero.split('.')
     
     if comprobacion[1] != ('smil'):
         sys.exit("Introducido un fichero distinto a .smil")
     
+
+    smallsmilhandler.manejador(fichero)
     
-    fichero = open(ficherosmil, 'r')
-    datos = fichero.read()
-    print(datos)
+    
+    
+ 
+    
