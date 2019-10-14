d9f3e8d2d370478c81f55bee72c4d71e38dc8245
anapgh <ap.garcia.2016@alumnos.urjc.es>
2019-10-14 16:11:45 +0200

    corregido con pep8

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -5,45 +5,43 @@ from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
 
-class SmallSMILHandler(ContentHandler): 
-   
-    def __init__(self):       
-        self.lista = []
-        self.elementDict = {
+class SmallSMILHandler(ContentHandler):
 
-        "root-layout": ["width", "height", "background-color"],
-        "region": ["id", "top", "left", "right"], 
-        "img" : ["src", "region", "begin", "dur"],
-        "audio" : ["src", "begin", "dur"], 
-        "textstream" : ["src", "region"]
-                            }
-    
+    def __init__(self):
+        self.lista = []
+        self.elemDict = {
+
+                        "root-layout": ["width", "height", "background-color"],
+                        "region": ["id", "top", "left", "right"],
+                        "img": ["src", "region", "begin", "dur"],
+                        "audio": ["src", "begin", "dur"],
+                        "textstream": ["src", "region"]
+                        }
+
     def startElement(self, name, attrs):
         """
         Método que se llama cuando se abre una etiqueta
         """
-        if name in self.elementDict:
+        if name in self.elemDict:
             # De esta manera tomamos los valores de los atributos
             dicc = {}
             dicc['etiqueta'] = name
-            for atributo in self.elementDict[name]:
+            for atributo in self.elemDict[name]:
                 dicc[atributo] = attrs.get(atributo, "")
             self.lista.append(dicc)
-            
-                
-            
-            
+
     def get_tags(self):
         return(self.lista)
-            
-        
+
+
 def manejador(ficherosmil):
     parser = make_parser()
     cHandler = SmallSMILHandler()
     parser.setContentHandler(cHandler)
     parser.parse(open(ficherosmil))
     lista = cHandler.get_tags()
-    print(lista)
+    return(lista)
+
 
 if __name__ == "__main__":
     """
